14412cb1046675c7a87063da406893c08be676bf
Andy Brody <andy@abrody.com>
2011-10-04 17:41:45 -0400

    Parse integer correctly.

--- etc/passforge.py
+++ etc/passforge.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         sys.exit(1)
 
     if len(sys.argv) > 4:
-        length = sys.argv[4]
+        length = int(sys.argv[4])
     else:
         length = 16
     print generate(sys.argv[1], sys.argv[2], int(sys.argv[3]), length)
